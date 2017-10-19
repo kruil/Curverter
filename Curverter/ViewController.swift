c17f153f490b1bb774c315d40b5bfb6b50d45e48
Ilya Krupko <kruiller@gmail.com>
2017-10-19 10:31:49 +0300

    Initial Commit

---
+++ Curverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Curverter
+//
+//  Created by Илья Крупко on 19.10.17.
+//  Copyright © 2017 Илья Крупко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
